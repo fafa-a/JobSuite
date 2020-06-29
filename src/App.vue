6bfb13c284aa36268e4f2b112ac098e28ec6ad7b
fafa-a <fablelong@yahoo.fr>
2020-06-29 23:42:21 +0200

    add vuetify and edit headerH1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <div id="app">
-    <HeaderH1 msg="job suite" />
-    <JobForm></JobForm>
-  </div>
+  <v-app>
+    <v-content>
+      <headerH1 msg="Job suite" />
+      <jobForm />
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 import HeaderH1 from "./components/HeaderH1.vue";
-import JobForm from "./components/JobForm.vue";
-
+import JobForm from "./components/JobForm";
 export default {
   name: "App",
+
   components: {
     HeaderH1,
     JobForm,
   },
+
+  data: () => ({
+    //
+  }),
 };
 </script>
-
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Archivo+Black&family=Montserrat&family=Roboto&display=swap");
-
 #app {
   margin-top: 40px;
 }
