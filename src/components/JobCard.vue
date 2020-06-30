66728f04877943917ebeb78f641bd56cff932bad
fafa-a <fablelong@yahoo.fr>
2020-06-30 15:27:58 +0200

    card-actions v1.1

--- src/components/JobCard.vue
+++ src/components/JobCard.vue
@@ -22,52 +22,23 @@
       </div>
     </v-card-text>
     <v-divider class="mt-6 mx-4"></v-divider>
-    <v-card-actions class="d-flex justify-space-around">
-      <v-btn
-        :href="data.URLJobOffer"
-        target="_blank"
-        text
-        color="deep-purple accent-4"
-      >
-        Job Page
-      </v-btn>
-      <v-btn text color="green accent-4">
-        Edit
-      </v-btn>
-      <v-btn text color="red accent-4">
-        Delete
-      </v-btn>
-    </v-card-actions>
-    <v-divider class="mt-6 mx-4"></v-divider>
-    <v-card-actions class="d-flex justify-space-between">
-      <span>{{ date }} </span>
-      <span>
-        <v-icon
-          @click="next"
-          :href="data.URLJobOffer"
-          target="_blank"
-          color="green darken-2"
-          dark
-          left
-          >fas fa-external-link-alt</v-icon
-        >
-        <v-icon color="orange darken-2" dark left>far fa-edit</v-icon>
-        <v-icon color="red darken-2" dark left>far fa-trash-alt</v-icon>
-      </span>
-    </v-card-actions>
-    <v-divider class="mt-6 mx-4"></v-divider>
     <v-card-actions>
       <v-card-text>
-        <v-chip class="mr-1" @click="lights">
-          <v-icon left>fas fa-external-link-alt</v-icon>
+        <v-chip
+          :href="data.URLJobOffer"
+          target="_blank"
+          class="mr-1"
+          @click="lights"
+        >
+          <v-icon color="green darken-1" left>fas fa-external-link-alt</v-icon>
           Job page
         </v-chip>
         <v-chip class="mr-1" @click="alarm">
-          <v-icon left>far fa-edit</v-icon>
+          <v-icon color="orange darken-1" left>far fa-edit</v-icon>
           Edit
         </v-chip>
-        <v-chip @click="blinds">
-          <v-icon left>far fa-trash-alt</v-icon>
+        <v-chip @click="blinds" :elevation="14">
+          <v-icon color="red darken-1" left>far fa-trash-alt</v-icon>
           Delete
         </v-chip>
       </v-card-text>
@@ -87,4 +58,14 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.theme--light {
+  background: #fff !important;
+}
+.v-chip--active {
+  background: #f1f1f1f1 !important;
+}
+.v-chip {
+  box-shadow: 2px 2px 4px #d1d9e6, -1px -1px 2px #fff;
+}
+</style>
